f7282ffbbdfd631af32950f2d76494795160562b
ingframin <framin2@gmail.com>
2020-04-16 23:56:33 +0200

    -

--- channel.h
+++ channel.h
@@ -1,4 +1,17 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
+#include<stdint.h>
+#include<stdbool.h>
+#include "drone.h"
+#include "vec2.h"
+/*Broadcasts are handled with publish subscribe*/
+typedef struct{
+    uint32_t id;
+    vec2 pos;
+    vec2 speed;
+}Message;
+
+
+//How to manage this in such a way that it can be parallelized?
 
 #endif
