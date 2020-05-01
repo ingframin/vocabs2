af2fcb56ab9a44302d1f32d7b627f1f5ebe3f52e
ingframin <framin2@gmail.com>
2020-05-01 20:42:57 +0200

    small change to v2_rotate

--- vec2.c
+++ vec2.c
@@ -5,17 +5,18 @@
 
 double v2_mod(vec2 v)
 {
-  return sqrt(v.x * v.x + v.y * v.y);
+  return hypot(v.x,v.y);
 }
 
 vec2 v2_rotate(vec2 v, double angle)
 {
-  double m = v2_mod(v);
+  
+  double m = hypot(v.x,v.y);
   vec2 ret;
   ret.x = v.x * cos(angle) - v.y * sin(angle);
   ret.y = v.x * sin(angle) + v.y * cos(angle);
-  vec2 tmp = v2_norm(ret);
-  ret = v2_prodK(tmp, m);
+  double retm = hypot(ret.x,ret.y);
+  ret = v2_prodK(ret, m/retm);
 
   return ret;
 }
@@ -23,7 +24,7 @@ vec2 v2_rotate(vec2 v, double angle)
 vec2 v2_norm(vec2 v)
 {
   vec2 n;
-  double m = v2_mod(v);
+  double m = hypot(v.x,v.y);
   n.x = v.x / m;
   n.y = v.y / m;
   return n;
@@ -69,5 +70,5 @@ vec2 v2_prodK(vec2 v, double k)
 double v2_distance(vec2 v1, vec2 v2)
 {
   vec2 res = v2_sub(v1, v2);
-  return v2_mod(res);
+  return hypot(res.x,res.y);
 }
