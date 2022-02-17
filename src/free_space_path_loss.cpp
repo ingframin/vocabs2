65671fe709f251fa19180afcc4d7cd8ebf58cb8e
ingframin <framin2@gmail.com>
2022-02-17 12:28:20 +0100

    Added free space path loss and path loss interface

---
+++ src/free_space_path_loss.cpp
@@ -0,0 +1,27 @@
+#include "pathloss.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+FreeSpacePL::FreeSpacePL(double frequency, double bandwidth, double gain_tx, double gain_rx, double noise_level){
+    this->frequency = frequency*1e6;
+    this->bandwidth = bandwidth;
+    this->gain_tx = gain_tx;
+    this->gain_rx = gain_rx;
+    this->noise_level = noise_level;
+    base_PL = 20*log10(frequency)+20*log10(4*M_PI/C);
+}
+
+double FreeSpacePL::loss_dB(double distance){
+    return 20*log10(distance)+base_PL;
+}
+
+double receivedPower(double distance, double transmitPower){
+    return transmitPower+gain_tx+gain_rx-loss_dB(distance);
+}
+
+double SNR(double distance, double transmitPower){
+    return receivedPower(distance,transmitPower)-noise_level;
+}
---
+++ src/free_space_path_loss.h
@@ -0,0 +1,18 @@
+#ifndef FS_PATHLOSS_H
+#define FS_PATHLOSS_H
+#include "pathloss.h"
+
+class FreeSpacePL: public PathLoss
+{
+public:
+    FreeSpacePL(double frequency, double bandwidth, double gain_tx, double gain_rx);
+    double loss_dB(double distance);
+    double receivedPower(double distance, double transmitPower);
+    double SNR(double distance, double transmitPower);
+private:
+    double gain_tx;
+    double gain_rx;
+    double base_PL;
+}
+
+#endif
