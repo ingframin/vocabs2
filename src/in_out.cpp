4bcb41a47a6ee74ba362cbae109434af47b521b0
ingframin <framin2@gmail.com>
2022-03-06 11:52:05 +0100

    Added input parsing and started refactoring of main.cpp

--- src/in_out.cpp
+++ src/in_out.cpp
@@ -37,3 +37,83 @@ void saveResults(const std::string& filename, char prob, const std::vector<doubl
     
     results.close();
 }
+
+SimConfig parseInputParameters(const std::vector<std::string>& params){
+    SimConfig sc;
+    for(auto p:params){
+        std::cout<<p<<std::endl;
+    }
+    return sc;
+}
+
+SimConfig readConfigFromFile(const std::string& filename){
+    SimConfig sc;
+    std::string tmp;
+    std::ifstream file(filename);
+    //This is really ugly but I don't know how to do it better.
+    //For now: good enough!
+    while(std::getline(file, tmp)){
+        if(tmp.find("//")!=std::string::npos){
+            continue;
+        }
+        std::stringstream line2stream(tmp);
+        std::string head;
+        line2stream >> head;
+        
+        if(head == "num_drones"){
+            line2stream >> sc.num_drones;
+            
+        }
+        else if(head == "drone_size"){
+            line2stream >> sc.drone_size;
+            
+        }
+        else if(head == "drone_speed"){
+            line2stream >> sc.drone_speed;
+            
+        }
+        else if(head == "width"){
+            line2stream >> sc.width;
+            
+        }
+        else if(head == "height"){
+            line2stream >> sc.height;
+            
+        }
+        else if(head == "start_rate"){
+            line2stream >> sc.start_rate;
+            
+        }
+        else if(head == "stop_rate"){
+            line2stream >> sc.stop_rate;
+            
+        }
+        else if(head == "step_rate"){
+            line2stream >> sc.step_rate;
+            
+        }
+        else if(head == "pos_error_dev"){
+            line2stream >> sc.pos_error_dev;
+            
+        }
+        else if(head == "height"){
+            line2stream >> sc.height;
+            
+        }
+        else if(head == "path_loss_model"){
+            line2stream >> sc.path_loss_model;
+            
+        }
+        else if(head == "dt"){
+            line2stream >> sc.dt;
+            
+        }
+        else if(head == "iterations"){
+            line2stream >> sc.iterations;
+            
+        }        
+
+    }
+    
+    return sc;
+}
--- src/in_out.h
+++ src/in_out.h
@@ -3,5 +3,22 @@
 #include <vector>
 #include <string>
 
+struct SimConfig{
+    uint64_t num_drones = 2;
+    double drone_size = 1.0;
+    double drone_speed = 20.0;
+    uint64_t width = 1000;
+    uint64_t height = 1000;
+    double start_rate = 1E-3;
+    double stop_rate = 20.0;
+    double step_rate = 0.25;
+    double pos_error_dev = 0;
+    std::string path_loss_model = "No Loss";
+    double dt = 1E-3;
+    uint32_t iterations = 20000;
+};
+
+SimConfig readConfigFromFile(const std::string& filename);
 void saveResults(const std::string& filename, char prob,const std::vector<double>& rates, const std::vector<long>& collisions, double error, double speed,double iterations, double loss);
+SimConfig parseInputParameters(const std::vector<std::string>& params);
 #endif
