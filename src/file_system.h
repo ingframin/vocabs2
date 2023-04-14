a0d80d28522017d8d76f139fb3e691ecb191d1e5
Franco Minucci <framin2@gmail.com>
2023-04-15 00:30:03 +0200

    Changed fseek and ftell to fseeko and ftello to use 64bit offsets

--- src/file_system.h
+++ src/file_system.h
@@ -1,8 +1,8 @@
 #ifndef FILE_SYSTEM_H
 #define FILE_SYSTEM_H
-#define FILE_SIZE(fp, size)  fseek(fp,0L,SEEK_END);\
-    size = ftell(fp);\
-    fseek(fp,0L,SEEK_SET);
+#define FILE_SIZE(fp, size)  fseeko(fp,0L,SEEK_END);\
+    size = ftello(fp);\
+    fseeko(fp,0L,SEEK_SET);
 #include <stdint.h>
 
 typedef struct content{
